7fe9a8c47719b6401288eb6a7987eb9a2dc5147a
jdsmith104 <jdsmith104@outlook.com>
2025-07-24 22:43:54 +0100

    Add video opener

---
+++ src/app/info.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function OpeningVideo() {
+    return (
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/FwMyLui2nrM?si=0q_h2NtL2kKCNtwC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
+
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 import Image from "next/image";
+import OpeningVideo from "./info";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div style={{ margin: "auto" }}>
-          <Image
-            aria-hidden
-            src="/ring.svg"
-            alt="File icon"
-            width={160}
-            height={160}
-            className="loader"
-          />
-        </div>
-        <div>
-          <text style={{ textAlign: 'center' }}>Coming soon...</text>
-        </div>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start basic">
+        <OpeningVideo
+        />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
