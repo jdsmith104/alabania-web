f87b6d56b1aef506fca449511c27d3177022798c
jdsmith104 <jdsmith104@outlook.com>
2025-07-25 01:21:54 +0100

    Update about text

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -5,12 +5,12 @@ export default function About() {
     return (
         <div>
             <div className={styles["about-page"]}  >
-                <h2>Welcome to Alabania</h2>
-                <p>Alabania is the slightly wacky brain child of Emily, Ethan, and Jacob - dreamed up to celebrate the inevitable engagement of Ala and Harry after 9 years of top-tier coupledom! (Yes, nine. We checked the calendar twice.)</p>
-                <p>Thank you for visiting Alabania. We hope you enjoy your stay!</p>
+                <h2>Welcome to Halabania</h2>
+                <p>Halabania is the slightly wacky brain child of Emily, Ethan, and Jacob - dreamed up to celebrate the inevitable engagement of Ala and Harry after 9 years of top-tier coupledom! (Yes, nine. We checked the calendar twice.)</p>
+                <p>Thank you for visiting Halabania. We hope you enjoy your stay!</p>
                 <footer className="mt-4">
                     <p>Created by Copilot and powered by AWS (jk)</p>
-                    <p>© 2025 Alabania</p>
+                    <p>© 2025 Halabania</p>
                 </footer>
             </div>
             <HomeLink />
